8c3f122efd69ff8947c54b3c79326921f13b935c
Asse <you@example.com>
2023-02-13 12:26:50 +0300

    dfghj

--- movie.js
+++ movie.js
@@ -3,16 +3,16 @@ const actorarray = [
   { name: "bernard", age: 1987 },
   { name: "andrew", age: 1980 },
 ];
-actorarray.forEach((element, i) => {
-  const section = document.querySelector(".section");
-  console.log(main);
-
-  const card = document.createElement("div");
-  card.classList = "card";
-
-  const moviecard = `
-<img src=""> <div class="info"> <h3> ${actorarray[i].name}</h3>
-<span>${actorarray[i].age}</span> </div>`;
-  card.innerHTML += moviecard;
-  section.appendChild(card);
+let moviecard = ``;
+actorarray.forEach(function (params) {
+  moviecard =
+    moviecard +
+    `
+<div class="info">
+<h3> ${params.name}</h3>
+<span>${params.age}</span>
+ </div>`;
 });
+
+const sections = document.querySelector(".section");
+sections.innerHTML = moviecard;
